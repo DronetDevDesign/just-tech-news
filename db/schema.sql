06ea5ecf097cbcd2c1fc4b54bd682d1d41851d00
Ron Dronet <ron@dronetdevdesign.com>
2022-08-22 11:12:41 -0500

    updated files

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,3 @@
--- Active: 1660403566821@@127.0.0.1@3306@just_tech_news_db
-DROP DATABASE IF EXISTS just_tech_news_db;
+DROP DATABASE IF EXISTS  just_tech_news_db;
 
-CREATE DATABASE just_tech_news_db;
+CREATE DATABASE  just_tech_news_db;
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote, Comment } = require('../../models');
+const { Post, User, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -15,7 +15,6 @@ router.get('/', (req, res) => {
     ],
     order: [['created_at', 'DESC']],
     include: [
-      // include the Comment model here:
       {
         model: Comment,
         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
@@ -51,6 +50,14 @@ router.get('/:id', (req, res) => {
     ],
     include: [
       {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
         model: User,
         attributes: ['username']
       }
@@ -84,33 +91,13 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-  Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id
-  }).then(() => {
-    // then find the post we just voted on
-    return Post.findOne({
-      where: {
-        id: req.body.post_id
-      },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
-        [
-          sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-          'vote_count'
-        ]
-      ]
-    })
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  });
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote, Comment, User })
+    .then(updatedVoteData => res.json(updatedVoteData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
@@ -157,4 +144,3 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
-
